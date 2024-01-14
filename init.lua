20ab02ff34c1500f9d564d6dab2f250793e74403
Kayushkin <vvkayushkin@gmail.com>
2024-01-13 20:15:32 -0800

    turned on py rust and html server probably

--- init.lua
+++ init.lua
@@ -540,10 +540,10 @@ require('mason-lspconfig').setup()
 local servers = {
   -- clangd = {},
   -- gopls = {},
-  -- pyright = {},
-  -- rust_analyzer = {},
-  -- tsserver = {},
-  -- html = { filetypes = { 'html', 'twig', 'hbs'} },
+  pyright = {},
+  rust_analyzer = {},
+  tsserver = {},
+  html = { filetypes = { 'html', 'twig', 'hbs'} },
 
   lua_ls = {
     Lua = {
